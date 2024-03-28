e4e2f276b3e34a7b85be0083ed0f21286f9570b6
Pampita Bhowmik <103371734+Pampita22@users.noreply.github.com>
2024-03-28 17:05:14 +0530

    Updated Solution - GfG to GitHub

--- Easy/Move all negative elements to end/move-all-negative-elements-to-end.cpp
+++ Easy/Move all negative elements to end/move-all-negative-elements-to-end.cpp
@@ -8,24 +8,26 @@ class Solution{
     void segregateElements(int arr[],int n)
     {
         // Your code goes here
-        vector<int>ans;
+        vector<int> ans;
         for(int i=0;i<n;i++)
         {
             if(arr[i]>=0)
-            ans.push_back(arr[i]);
+            {
+                ans.push_back(arr[i]);
+            }
         }
         for(int i=0;i<n;i++)
         {
             if(arr[i]<0)
-            ans.push_back(arr[i]);
+            {
+                ans.push_back(arr[i]);
+            }
         }
         for(int i=0;i<n;i++)
         {
             arr[i]=ans[i];
         }
-        
     }
-    
 };
 
 //{ Driver Code Starts.
