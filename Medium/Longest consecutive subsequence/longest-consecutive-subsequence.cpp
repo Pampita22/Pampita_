c037571e17237a549b4182f0933f5f635d07cb05
Pampita Bhowmik <103371734+Pampita22@users.noreply.github.com>
2024-04-08 10:21:12 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest consecutive subsequence/longest-consecutive-subsequence.cpp
+++ Medium/Longest consecutive subsequence/longest-consecutive-subsequence.cpp
@@ -13,32 +13,33 @@ class Solution{
     int findLongestConseqSubseq(int arr[], int N)
     {
       //Your code here
-      int n=N;
-      if(n==0)
-      return 0;
-      
-      int longest=1;
-      unordered_set<int> st;
-      for(int i=0;i<n;i++)
-      {
-          st.insert(arr[i]);
-      }
-      
-      for(auto it:st)
-      {
-          if(st.find(it-1)==st.end())
-          {
-              int cnt=1;
-              int x=it;
-              while(st.find(x+1)!=st.end())
-              {
-                  x += 1;
-                  cnt=cnt+1;
-              }
-              longest =max(longest,cnt);
-          }
-      }
-      return longest;
+     int minele=INT_MAX;
+     int maxele=INT_MIN;
+     map<int,bool> mp;
+     
+     for(int i=0;i<N;i++)
+     {
+         mp[arr[i]]=true;
+         if(arr[i]<minele)
+         minele=arr[i];
+         if(arr[i]>maxele)
+         maxele=arr[i];
+     }
+     int count=0;
+     int ans=INT_MIN;
+     for(int i=minele;i<=maxele;i++)
+     {
+         if(mp.find(i)!=mp.end())
+         {
+             count++;
+             ans=max(ans,count);
+         }
+         else
+         {
+             count=0;
+         }
+     }
+     return ans;
     }
 };
 
