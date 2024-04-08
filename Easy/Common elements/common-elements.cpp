b3d86ee7941038f0909b3847d8cc7b51be3fcf93
Pampita Bhowmik <103371734+Pampita22@users.noreply.github.com>
2024-04-08 12:53:29 +0530

    Updated Solution - GfG to GitHub

--- Easy/Common elements/common-elements.cpp
+++ Easy/Common elements/common-elements.cpp
@@ -10,31 +10,41 @@ class Solution
        vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
         {
             //code here.
-    set<int>st;
-    vector<int>ans;
-    int i,j,k;
-    i=j=k=0;
-    while(i<n1 && j<n2 && k<n3){
-        if(A[i]==B[j] && B[j]==C[k]){
-            st.insert(A[i]);
-            i++,j++,k++;
+            set<int> st;
+            vector<int> res;
+            
+            int i=0;
+            int j=0;
+            int k=0;
+            while(i<n1&&j<n2&&k<n3)
+            {
+                if(A[i]==B[j]&&B[j]==C[k])
+                {
+                    st.insert(A[i]);
+                    i++;j++;k++;
+                }
+                else if(A[i]<B[j])
+                {
+                    i++;
+                }
+                else if(B[j]<C[k])
+                {
+                    j++;
+                }
+                else
+                {
+                    k++;
+                }
+                
+            }
+            for(auto i:st)
+            {
+                res.push_back(i);
+            }
+            return res;
         }
-        else if(A[i]<B[j]){
-            i++;
-        }
-        else if(B[j]<C[k]){
-            j++;
-        }
-        else{
-            k++;
-        }
-    }
-    for(auto i:st){
-        ans.push_back(i);
-    }
-    return ans;
-        }
-
+            
+            
 };
 
 //{ Driver Code Starts.
