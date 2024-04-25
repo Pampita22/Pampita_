40dd7d7fab2ec4bc770311b529cfb7d50a1370a1
Pampita Bhowmik <103371734+Pampita22@users.noreply.github.com>
2024-04-25 22:58:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Kadane's Algorithm/kadanes-algorithm.cpp
+++ Medium/Kadane's Algorithm/kadanes-algorithm.cpp
@@ -12,14 +12,17 @@ class Solution{
     long long maxSubarraySum(int arr[], int n){
         
         // Your code here
-        long long sum=0,maxi=arr[0];
-        for(int i=0;i<n;i++)
-        {
-            sum=sum+arr[i];
-            maxi=max(maxi,sum);
-            if(sum<0)sum=0;
-        }
-        return maxi;
+       long long sum=0,maxi=arr[0];
+       for(int i=0;i<n;i++)
+       {
+           sum=sum+=arr[i];
+           maxi=max(maxi,sum);
+           if(sum<0)
+           {
+               sum=0;
+           }
+       }
+       return maxi;
         
     }
 };
